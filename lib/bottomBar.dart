3f9d96134ac3b9388f50643e3b9fecf8e59238cf
americano <rlagurpoker@gmail.com>
2023-05-09 19:12:59 +0900

    바텀 바 파일 업로드

---
+++ lib/bottomBar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'registerMeeting.dart';
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        IconButton(icon: Image.asset("assets/images/Bottombar_first.png"),onPressed: () {},),
+        IconButton(icon: Image.asset("assets/images/Bottombar_second.png"),onPressed: () {},),
+        IconButton(icon: Image.asset("assets/images/Bottombar_center.png"),onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(
+                  builder: (context) => RegisterMeeting()));
+          },
+        ),
+        IconButton(icon: Image.asset("assets/images/Bottombar_third.png"),onPressed: () {},),
+        IconButton(icon: Image.asset("assets/images/Bottombar_fourth.png"),onPressed: () {},),
+      ],
+    );
+  }
+}
